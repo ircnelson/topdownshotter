d4aae386c6ca8e427f80a2614fd25e111617de3b
Nelson Junior <Nelson Junior>
2016-10-16 01:47:17 -0300

    player movements

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerComponent))]
+public class Player : MonoBehaviour {
+
+    public float MoveSpeed = 5;
+
+    private Camera _viewCamera;
+    private PlayerComponent _controller;
+	
+	void Start ()
+    {
+        _controller = GetComponent<PlayerComponent>();
+
+        _viewCamera = Camera.main;
+	}
+	
+	void Update ()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * MoveSpeed;
+
+        _controller.Move(moveVelocity);
+
+        Ray ray = _viewCamera.ScreenPointToRay(Input.mousePosition);
+
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+
+            Debug.DrawLine(ray.origin, point, Color.red);
+
+            _controller.LookAt(point);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerComponent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerComponent : MonoBehaviour {
+
+    private Vector3 _velocity;
+    private Rigidbody _rigidbody;
+    
+	void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();	
+	}
+	
+	void FixedUpdate ()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _velocity * Time.fixedDeltaTime);
+	}
+
+    public void Move(Vector3 velocity)
+    {
+        _velocity = velocity;
+    }
+
+    public void LookAt(Vector3 point)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
+
+        transform.LookAt(heightCorrectedPoint);
+    }
+}
